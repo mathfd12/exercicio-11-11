7d948eddab56206126fd6a9483de8e30b7a43401
Anna Beatriz Serikyaku <annabserikyaku@gmail.com>
2019-11-14 21:26:48 -0300

    Exemplo de for

---
+++ for.py
@@ -0,0 +1,11 @@
+lista_nomes =  ["miau","eu","tu"]
+
+#for nome in lista_nomes:
+    #print(nome)
+#else :
+ #   print("Todos os nomes da lista foram imprimidos")
+
+#for item in range(0,5):
+ #   print(item)
+for item in range(0, 5, 2):
+    print(item)
